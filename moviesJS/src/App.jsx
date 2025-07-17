17a46d56f6e35480b86fa2141e054441ea68f729
Adin Lamport <adin.lamport@verizon.net>
2025-07-17 17:06:53 +0900

    Creating 2 pages to display movies

--- moviesJS/src/App.jsx
+++ moviesJS/src/App.jsx
@@ -1,20 +1,31 @@
-import './App.css'
+import './App.css';
+import MovieCard from './components/MovieCard';
 
 function App() {
+  const movieNumber = 1;
+
 
   return (
     <>
-      <Text />
-      <Text />
+      {/* {movieNumber === 1 ? (
+        <MovieCard movie={{title: "Adin's Bio", release_date: "2025"}}/>
+        ) : (
+        <MovieCard movie={{title: "Crazy Rich Asians", release_date: "2024"}}/>
+        )} */}
+
+      {movieNumber === 1 && <MovieCard movie={{title: "Adin's Film", release_data: "2025"}} />}
+      
+      {/* <Text display="Displaying!"/>
+      <Text display="Hello!"/> */}
     </>
 
   )
 }
 
-function Text() {
+function Text({display}) {
   return (
     <div>
-      <p>Hello World!</p>
+      <p>{display}</p>
     </div>
   )
 }
