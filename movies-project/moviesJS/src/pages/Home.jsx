641955b77e700fe6d35c0c86536f169556db13ed
Adin Lamport <adin.lamport@verizon.net>
2025-07-19 23:55:45 +0900

    Movie List Update

--- movies-project/moviesJS/src/pages/Home.jsx
+++ movies-project/moviesJS/src/pages/Home.jsx
@@ -7,6 +7,9 @@ function Home() {
     {id: 3, title: "How to Lose a Guy in 10 Days", release_date: "2003"},
     {id: 4, title: "Crazy Rich Asians", release_date: "2018"},
     {id: 5, title: "No Reservations", release_date: "2007"},
+    {id: 6, title: "Burnt", release_date: "2015"},
+    {id: 7, title: "Ratatouille", release_date: "2007"},
+    {id: 8, title: "20th Century Girl", release_date: "2022"},
   ]
 
   const handleSearch = () => {
